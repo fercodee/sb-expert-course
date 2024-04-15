43fda443adac67a37ab4b9970ce29b2d56d36188
fercodee <devbyfull@gmail.com>
2024-04-14 21:41:49 -0300

    fix: query to find itens

--- src/main/java/github/com/fercodee/domain/repositories/Pedidos.java
+++ src/main/java/github/com/fercodee/domain/repositories/Pedidos.java
@@ -5,11 +5,15 @@ import java.util.Optional;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import github.com.fercodee.domain.entity.Cliente;
 import github.com.fercodee.domain.entity.Pedido;
 
 public interface Pedidos extends JpaRepository<Pedido, Integer>{
     List<Pedido> findByCliente(Cliente cliente);
-    Optional<Pedido> findByIdFetchItens(Integer id);
+
+    @Query(" select p from Pedido p left join fetch p.itens where p.id = :id ")
+    Optional<Pedido> findByIdFetchItens(@Param("id") Integer id);
 }
